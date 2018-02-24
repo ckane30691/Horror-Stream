f9c792737690e9ea594edc397727860b2cd047e1
Cory Kane <ckane30691@gmail.com>
2018-02-24 13:01:47 -0800

    Write google auth routes

--- app.js
+++ app.js
@@ -22,3 +22,42 @@ const app = express();
 app.engine('html', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/static_pages');
+
+//middleware config
+app.use(passport.initialize());
+
+const logger = require('morgan');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Request-Headers", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
+app.get('/api/authentication/google/start',
+    passport.authenticate(
+      'google',
+      { session: false, scope: ['openid', 'profile', 'email'] })
+    );
+    
+app.get('/api/authentication/google/redirect',
+    passport.authenticate(
+      'google',
+      { session: false }),
+      generateUserToken
+    );
