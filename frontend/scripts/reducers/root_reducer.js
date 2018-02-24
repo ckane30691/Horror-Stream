4f1bf9def4df7b9cd50d4103ccd006920d572a9f
Cory Kane <ckane30691@gmail.com>
2018-02-24 14:37:07 -0800

    Write session_reducer

--- frontend/scripts/reducers/root_reducer.js
+++ frontend/scripts/reducers/root_reducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-// import session from './session_reducer';
+import session from './session_reducer';
 
 const RootReducer = combineReducers({
-  // session,
+  session,
 });
 
 export default RootReducer;
