cdeb94c6945a4a00bad64bd2d8c3be040a50085d
Cory Kane <ckane30691@gmail.com>
2018-02-24 12:30:38 -0800

    set up google passport config

---
+++ authentication/google.js
@@ -0,0 +1,10 @@
+const passport = require('passport');
+const passportGoogle = require('passport-google-oauth');
+const config = require('../config');
+const users = require('../users');
+
+const passportConfig = {
+    clientID: config.get('authentication.google.clientId'),
+    clientSecret: config.get('authentication.google.clientSecret'),
+    callbackURL: 'https://node-react-test1.herokuapp.com/api/authentication/google/redirect'
+};
